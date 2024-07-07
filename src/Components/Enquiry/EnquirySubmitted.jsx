5e323fbdb99bfe66a8e0c9a2211def6ab8f1b21a
ar3hma786 <arahmanhussain786@gmail.com>
2024-07-08 00:20:54 +0400

    changes

--- src/Components/Enquiry/EnquirySubmitted.jsx
+++ src/Components/Enquiry/EnquirySubmitted.jsx
@@ -5,7 +5,7 @@ import { BiArrowBack } from 'react-icons/bi'; // Import BiArrowBack from react-i
 function EnquirySubmitted() {
   return (
     <div className="relative h-auto sm:h-[20rem] bg-gradient-to-l from-green-400 to-blue-200 flex flex-col items-center justify-center py-8 px-4 sm:px-6 lg:px-8">
-      <Link to="/enquiry-form" className="absolute top-12 left-12 bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 z-10">
+      <Link to="/enquiry-form" className="absolute top-12 left-12 bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 z-10 hidden sm:inline-flex">
         <BiArrowBack className="inline-block mr-2" />
       </Link>
       <div className="bg-gradient-to-l from-green-400 to-blue-200 bg-opacity-70 rounded-lg shadow-lg p-8 max-w-full w-full">
