f76a5919aef624e1dd71d9f37331371b149dc222
ar3hma786 <arahmanhussain786@gmail.com>
2024-07-08 00:16:13 +0400

    enquirysubmittedchanges

--- src/Components/Enquiry/EnquirySubmitted.jsx
+++ src/Components/Enquiry/EnquirySubmitted.jsx
@@ -7,7 +7,6 @@ function EnquirySubmitted() {
     <div className="relative h-auto sm:h-[20rem] bg-gradient-to-l from-green-400 to-blue-200 flex flex-col items-center justify-center py-8 px-4 sm:px-6 lg:px-8">
       <Link to="/enquiry-form" className="absolute top-12 left-12 bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 z-10">
         <BiArrowBack className="inline-block mr-2" />
-        Back
       </Link>
       <div className="bg-gradient-to-l from-green-400 to-blue-200 bg-opacity-70 rounded-lg shadow-lg p-8 max-w-full w-full">
         <div className="text-center text-gray-900">
